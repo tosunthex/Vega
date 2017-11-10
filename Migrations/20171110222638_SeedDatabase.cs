057f5921db542552b6951c25d251dc57486f83f7
tosunthex <leventsaatci@hotmail.com>
2017-11-10 14:52:14 -0800

    virtual pc push

---
+++ Migrations/20171110222638_SeedDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
