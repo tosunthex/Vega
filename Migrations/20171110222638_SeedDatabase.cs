c3d3b7d18f385a743baa9d808add87e5e3ed7051
levent <leventsaatci@hotmail.com>
2017-11-11 22:10:25 +0100

    SeedDatabase

--- Migrations/20171110222638_SeedDatabase.cs
+++ Migrations/20171110222638_SeedDatabase.cs
@@ -8,11 +8,27 @@ namespace vega.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("Insert Into Makes (Name) Values ('Make1')");
+            migrationBuilder.Sql("Insert Into Makes (Name) Values ('Make2')");
+            migrationBuilder.Sql("Insert Into Makes (Name) Values ('Make3')");
+            
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelA',(Select ID from Makes Where Name = 'Make1'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelB',(Select ID from Makes Where Name = 'Make1'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelC',(Select ID from Makes Where Name = 'Make1'))");
+            
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelA',(Select ID from Makes Where Name = 'Make2'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelB',(Select ID from Makes Where Name = 'Make2'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelC',(Select ID from Makes Where Name = 'Make2'))");
+            
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelA',(Select ID from Makes Where Name = 'Make3'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelB',(Select ID from Makes Where Name = 'Make3'))");
+            migrationBuilder.Sql("Insert Into Models (Name,MakeID) Values ('Make1-ModelC',(Select ID from Makes Where Name = 'Make3'))");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("Delete From Makes Where Name In ('Make1','Make2','Make3')");
+            migrationBuilder.Sql("Delete From Models Where ModelID In ( Select ID from Makes Where Name In ('Make1','Make2','Make3') )");
         }
     }
 }
